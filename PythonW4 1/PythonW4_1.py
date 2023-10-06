0c932b98c7745e8c157222ed6b602d36c2651460
ThatEffGuy <craig_fergy@hotmail.com>
2023-10-06 15:39:56 +0100

    Add project files.

---
+++ PythonW4 1/PythonW4_1.py
@@ -0,0 +1,10 @@
+print("\tWelcome to the 'Three-Year-Old Simulator'\n")
+print("This program simulates a conversation with a three-year-old child.")
+print("Try to stop the madness.\n")
+
+response = ""
+while response != "Because.":
+    response = input("Why?\n")
+
+print("Oh.  Okay.")
+
